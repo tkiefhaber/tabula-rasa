1af0600055547f2600536cc5120d86e46ccee48a
Lauren Voswinkel <lauren.voswinkel@livingsocial.com>
2014-04-30 10:14:44 -0400

    No jQuery, smaller package, better performance. :)

--- tabula-rasa.js
+++ tabula-rasa.js
@@ -1,1 +1,6 @@
-jQuery('a').attr('target', '_blank')
+var anchors = document.getElementsByTagName('a')
+  , length = anchors.length;
+
+for(var i = 0; i < length; i++){
+  anchors[i].setAttribute("target", "_blank");
+}
